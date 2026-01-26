5cca1059d8284140ff460b989677b0fc0e5193b5
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-26 12:13:37 +0530

    Speciality Menu

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -4,6 +4,12 @@ import dropdown_icon from "./dropdown_icon.svg";
 import group_profiles from "./group_profiles.png";
 import arrow_icon from "./arrow_icon.svg";
 import header_img from "./header_img.png";
+import Dermatologist from "./Dermatologist.svg";
+import Gastroenterologist from "./Gastroenterologist.svg";
+import General_physician from "./General_physician.svg";
+import Gynecologist from "./Gynecologist.svg";
+import Neurologist from "./Neurologist.svg";
+import Pediatricians from "./Pediatricians.svg";
 
 export const assets = {
   logo,
@@ -13,3 +19,30 @@ export const assets = {
   arrow_icon,
   header_img,
 };
+
+export const specialityData = [
+  {
+    speciality: "General physician",
+    image: General_physician,
+  },
+  {
+    speciality: "Gynecologist",
+    image: Gynecologist,
+  },
+  {
+    speciality: "Dermatologist",
+    image: Dermatologist,
+  },
+  {
+    speciality: "Pediatricians",
+    image: Pediatricians,
+  },
+  {
+    speciality: "Neurologist",
+    image: Neurologist,
+  },
+  {
+    speciality: "Gastroenterologist",
+    image: Gastroenterologist,
+  },
+];
