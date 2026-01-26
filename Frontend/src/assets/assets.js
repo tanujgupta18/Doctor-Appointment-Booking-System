39c78051677a67c06df5a35dea596efcc497d45d
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-26 11:53:05 +0530

    Header Section Final

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -1,9 +1,15 @@
 import logo from "./logo.svg";
 import profile_pic from "./profile_pic.png";
 import dropdown_icon from "./dropdown_icon.svg";
+import group_profiles from "./group_profiles.png";
+import arrow_icon from "./arrow_icon.svg";
+import header_img from "./header_img.png";
 
 export const assets = {
   logo,
   profile_pic,
   dropdown_icon,
+  group_profiles,
+  arrow_icon,
+  header_img,
 };
