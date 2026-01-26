55adc7c9d3b723e7f1ff2ecd5d74ab2a5ee0aae8
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-26 11:47:45 +0530

    Header Section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="mx-4 sm:mx-[10%]">
       <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 };
