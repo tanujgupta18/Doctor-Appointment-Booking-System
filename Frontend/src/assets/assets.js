3e2a4df201ef0f1ae594779158f448f27e03f2d1
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-26 10:33:49 +0530

    Navbar

---
+++ Frontend/src/assets/assets.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import profile_pic from "./profile_pic.png";
+import dropdown_icon from "./dropdown_icon.svg";
+
+export const assets = {
+  logo,
+  profile_pic,
+  dropdown_icon,
+};
