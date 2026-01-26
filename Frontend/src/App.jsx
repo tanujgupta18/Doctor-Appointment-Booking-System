d81918169bcbe09c3dbb140e64e6bf831d8cd273
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-26 15:44:34 +0530

    Footer Section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -11,6 +12,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+
+      <Footer />
     </div>
   );
 };
