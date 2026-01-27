ee61ede6ae017e90ebc710f04cef805bb84d1062
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-27 12:16:33 +0530

    Related Doctors

--- Frontend/src/components/RelatedDoctors.jsx
+++ Frontend/src/components/RelatedDoctors.jsx
@@ -1,19 +1,29 @@
-import { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 
-const TopDoctors = () => {
-  const navigate = useNavigate();
+const RelatedDoctors = ({ docId, speciality }) => {
   const { doctors } = useContext(AppContext);
+  const [relatedDoctor, setRelatedDoctor] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (doctors.length > 0 && speciality) {
+      const doctorsData = doctors.filter(
+        (doc) => doc.speciality === speciality && doc._id !== docId,
+      );
+      setRelatedDoctor(doctorsData);
+    }
+  }, [doctors, speciality, docId]);
 
   return (
-    <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
-      <h1 className="text-3xl font-medium">Top Doctors to Book</h1>
+    <div className="flex flex-col items-center gap-4 my-10 text-gray-900 md:mx-10">
+      <h1 className="text-3xl font-medium">Related Doctors</h1>
       <p className="sm:w-1/3 text-center text-sm">
         Simply browse through our extensive list of trusted doctors.
       </p>
       <div className="w-full grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4 pt-5 gap-y-6 px-3 sm:px-0">
-        {doctors.slice(0, 10).map((item, index) => (
+        {relatedDoctor.slice(0, 5).map((item, index) => (
           <div
             onClick={() => {
               navigate(`/appointment/${item._id}`);
@@ -51,4 +61,4 @@ const TopDoctors = () => {
   );
 };
 
-export default TopDoctors;
+export default RelatedDoctors;
--- Frontend/src/pages/Appointment.jsx
+++ Frontend/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
+import RelatedDoctors from "../components/RelatedDoctors";
 
 const Appointment = () => {
   const { docId } = useParams();
@@ -170,6 +171,9 @@ const Appointment = () => {
             Book an appointment
           </button>
         </div>
+
+        {/*----- Listing Related Doctors -----*/}
+        <RelatedDoctors docId={docId} speciality={docInfo.speciality} />
       </div>
     )
   );
