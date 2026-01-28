7800483e00d1b2716f6502b819c3bf17d41c10f8
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-28 19:50:22 +0530

    Initial Setup - Backend

---
+++ Backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/db.js";
+
+// app config
+const app = express();
+const PORT = process.env.PORT || 3000;
+connectDB();
+
+// middlewares
+app.use(cors());
+
+// api endpoints
+app.get("/", (req, res) => {
+  res.send("Api is working");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}/`);
+});
