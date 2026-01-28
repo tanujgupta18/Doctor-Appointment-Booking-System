6807f11cddc88a6b437dbed442b9a9c5f96f02fe
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-28 19:51:54 +0530

    Cloudinary Setup

--- Backend/server.js
+++ Backend/server.js
@@ -2,13 +2,16 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 // app config
 const app = express();
 const PORT = process.env.PORT || 3000;
 connectDB();
+connectCloudinary();
 
 // middlewares
+app.use(express.json());
 app.use(cors());
 
 // api endpoints
