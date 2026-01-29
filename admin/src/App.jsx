85cd6aee27696733c1d53c58c7ff6a0b694aa054
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-29 19:08:53 +0530

    Admin Login Setup

---
+++ admin/src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import Login from "./pages/Login";
+import { ToastContainer, toast } from "react-toastify";
+import { AdminContext } from "./context/AdminContext";
+
+const App = () => {
+  const { aToken } = useContext(AdminContext);
+
+  return aToken ? (
+    <div>
+      <ToastContainer />
+    </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
+  );
+};
+
+export default App;
