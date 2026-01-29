8282039210691abbdc9a9b2493e72cceb65b0fd7
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-29 19:21:43 +0530

    Admin Navbar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,13 +2,15 @@ import React, { useContext } from "react";
 import Login from "./pages/Login";
 import { ToastContainer, toast } from "react-toastify";
 import { AdminContext } from "./context/AdminContext";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
 
   return aToken ? (
-    <div>
+    <div className="bg-[#f8f9fd]">
       <ToastContainer />
+      <Navbar />
     </div>
   ) : (
     <>
---
+++ admin/src/assets/assets.js
@@ -0,0 +1,5 @@
+import admin_logo from "./admin_logo.svg";
+
+export const assets = {
+  admin_logo,
+};
