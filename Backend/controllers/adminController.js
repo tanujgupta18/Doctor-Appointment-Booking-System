0da382fa247fc7510f00736f3dd591f688840a8c
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-30 20:35:07 +0530

    Get all doctors API

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -101,4 +101,14 @@ const loginAdmin = async (req, res) => {
   }
 };
 
-export { addDoctor, loginAdmin };
+// API to get all doctors list
+const allDoctors = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({}).select("-password");
+    res.json({ success: true, doctors });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { addDoctor, loginAdmin, allDoctors };
