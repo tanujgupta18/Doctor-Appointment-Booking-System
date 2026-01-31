3ead34425a2da5262573a6fbface0a537e9a4521
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-31 12:42:07 +0530

    Doctor List API

--- Backend/controllers/doctorController.js
+++ Backend/controllers/doctorController.js
@@ -1,5 +1,6 @@
 import doctorModel from "../models/doctorModel.js";
 
+// API to Check Availablity
 const changeAvailablity = async (req, res) => {
   try {
     const { docId } = req.body;
@@ -15,4 +16,14 @@ const changeAvailablity = async (req, res) => {
   }
 };
 
-export { changeAvailablity };
+// API to get doctor list
+const doctorList = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({}).select(["-password", "-email"]);
+    res.json({ success: true, doctors });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { changeAvailablity, doctorList };
