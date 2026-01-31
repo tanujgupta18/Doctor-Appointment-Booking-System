460da1e7d7ea3c946226b70b0238bb67f5e360a3
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-01-31 12:43:06 +0530

    Doctors Data Fetch from DB

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,10 +10,12 @@ import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import MyProfile from "./pages/MyProfile";
 import MyAppointment from "./pages/MyAppointment";
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
   return (
     <div className="mx-4 sm:mx-[10%]">
+      <ToastContainer />
       <Navbar />
 
       <Routes>
--- Frontend/src/context/AppContext.jsx
+++ Frontend/src/context/AppContext.jsx
@@ -1,15 +1,36 @@
-import { createContext } from "react";
-import { doctors } from "../assets/assets";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const currency = "$";
+  const [doctors, setDoctors] = useState([]);
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const getDoctorsData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/doctor/list");
+
+      if (data.success) {
+        setDoctors(data.doctors);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
   const value = {
     doctors,
     currency,
   };
+
+  useEffect(() => {
+    getDoctorsData();
+  }, []);
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
