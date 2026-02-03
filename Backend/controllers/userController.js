e4d732eec35df49c2aa0e074d7086aae8b9171d5
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-02-03 15:43:52 +0530

    Razorpay Integration

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -5,6 +5,7 @@ import jwt from "jsonwebtoken";
 import { v2 as cloudinary } from "cloudinary";
 import doctorModel from "../models/doctorModel.js";
 import appointmentModel from "../models/appoinmentModel.js";
+import razorpay from "razorpay";
 
 // API to register user
 const registerUser = async (req, res) => {
@@ -216,6 +217,64 @@ const cancelAppointment = async (req, res) => {
   }
 };
 
+const razorpayInstance = new razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+// API to make payment
+const paymentRazorpay = async (req, res) => {
+  try {
+    const { appointmentId } = req.body;
+    const appointmentData = await appointmentModel.findById(appointmentId);
+
+    if (!appointmentData || appointmentData.cancelled) {
+      return res.json({
+        success: false,
+        message: "Appointment Cancelled or not found",
+      });
+    }
+
+    if (appointmentData.payment) {
+      return res.json({
+        success: false,
+        message: "Already Paid",
+      });
+    }
+
+    // creating Options for Payment methods
+    const options = {
+      amount: appointmentData.amount * 100,
+      currency: process.env.CURRENCY,
+      receipt: appointmentId,
+    };
+
+    // order creation
+    const order = await razorpayInstance.orders.create(options);
+    res.json({ success: true, order });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API to verify payment
+const verifyRazorpay = async (req, res) => {
+  try {
+    const { razorpay_order_id } = req.body;
+    const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id);
+
+    const appointmentId = orderInfo.receipt;
+
+    await appointmentModel.findByIdAndUpdate(appointmentId, {
+      payment: true,
+    });
+
+    res.json({ success: true, message: "Payment Successful" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
 export {
   registerUser,
   loginUser,
@@ -224,4 +283,6 @@ export {
   bookAppointment,
   listAppointment,
   cancelAppointment,
+  paymentRazorpay,
+  verifyRazorpay,
 };
--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -5,8 +5,10 @@ import {
   getProfile,
   listAppointment,
   loginUser,
+  paymentRazorpay,
   registerUser,
   updateProfile,
+  verifyRazorpay,
 } from "../controllers/userController.js";
 import authUser from "../middlewares/authUser.js";
 import upload from "../middlewares/multer.js";
@@ -25,5 +27,7 @@ userRouter.post(
 userRouter.post("/book-appointment", authUser, bookAppointment);
 userRouter.get("/appointments", authUser, listAppointment);
 userRouter.post("/cancel-appointment", authUser, cancelAppointment);
+userRouter.post("/payment-razorpay", authUser, paymentRazorpay);
+userRouter.post("/verifyRazorpay", authUser, verifyRazorpay);
 
 export default userRouter;
