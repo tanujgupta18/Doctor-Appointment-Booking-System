e3366234b26a60d3e5d250dc2fb5bca97063d676
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-02-04 12:13:06 +0530

    Changes

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { createContext, useState } from "react";
+import { toast } from "react-toastify";
 
 export const DoctorContext = createContext();
 
@@ -6,13 +8,37 @@ const DoctorContextProvider = (props) => {
   const [dToken, setDToken] = useState(
     localStorage.getItem("dToken") ? localStorage.getItem("dToken") : "",
   );
+  const [appointments, setAppointments] = useState([]);
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const getAppointments = async (req, res) => {
+    try {
+      const { data } = await axios.get(
+        backendUrl + "/api/doctor/appointments",
+        {
+          headers: { dToken },
+        },
+      );
+
+      if (data.success) {
+        setAppointments(data.appointments.reverse());
+        console.log(data.appointments.reverse());
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
   const value = {
     dToken,
     setDToken,
     backendUrl,
+    appointments,
+    setAppointments,
+    getAppointments,
   };
 
   return (
