461f65800cdc88ddb058c470321bfae0e0d1d954
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-02-04 09:59:02 +0530

    Doctors - Sidebar

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -2,12 +2,15 @@ import React, { useContext } from "react";
 import { AdminContext } from "../context/AdminContext";
 import { NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
+import { DoctorContext } from "../context/DoctorContext";
 
 const SideBar = () => {
   const { aToken } = useContext(AdminContext);
+  const { dToken } = useContext(DoctorContext);
 
   return (
     <div className="min-h-screen bg-white border-r">
+      {/* Admin's Sidebar */}
       {aToken && (
         <ul className="text-[#515151] mt-5">
           <NavLink
@@ -45,6 +48,40 @@ const SideBar = () => {
           >
             <img src={assets.people_icon} alt="people-icon" />
             <p>Doctors List</p>
+          </NavLink>
+        </ul>
+      )}
+
+      {/* Doctor's Sidebar */}
+      {dToken && (
+        <ul className="text-[#515151] mt-5">
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#f2f3ff] border-r-4 border-primary" : ""}`
+            }
+            to={"/doctor-dashboard"}
+          >
+            <img src={assets.home_icon} alt="home-icon" />
+            <p>Dashboard</p>
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#f2f3ff] border-r-4 border-primary" : ""}`
+            }
+            to={"/doctor-appointments"}
+          >
+            <img src={assets.appointment_icon} alt="appointment-icon" />
+            <p>Appointments</p>
+          </NavLink>
+
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#f2f3ff] border-r-4 border-primary" : ""}`
+            }
+            to={"/doctor-profile"}
+          >
+            <img src={assets.people_icon} alt="people-icon" />
+            <p>Profile</p>
           </NavLink>
         </ul>
       )}
