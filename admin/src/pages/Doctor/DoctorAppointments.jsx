024a11dc5110dc425476481b5365764dcdea7fd8
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-02-05 09:40:24 +0530

    Changes

--- admin/src/pages/Doctor/DoctorAppointments.jsx
+++ admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -63,7 +63,7 @@ const DoctorAppointments = () => {
             {item.cancelled ? (
               <p className="text-red-400 text-xs font-medium">Cancelled</p>
             ) : item.isCompleted ? (
-              <p className="text-green-500 text-xs font-medium">Cancelled</p>
+              <p className="text-green-500 text-xs font-medium">Completed</p>
             ) : (
               <div className="flex">
                 <img
