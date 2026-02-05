04ea1d2a4b9c908be3d468b4d9f0c77f24f83775
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-02-05 10:08:13 +0530

    Favicon Added

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -21,23 +21,30 @@ const Navbar = () => {
         alt="logo"
         className="w-44 cursor-pointer"
       />
-      <ul className="uppercase hidden md:flex items-start gap-5 font-medium">
+      <ul className="hidden md:flex items-center gap-5 font-medium">
         <NavLink to="/">
-          <li className="py-1">Home</li>
+          <li className="py-1 uppercase">Home</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
         <NavLink to="/doctors">
-          <li className="py-1">All Doctors</li>
+          <li className="py-1 uppercase">All Doctors</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
         <NavLink to="/about">
-          <li className="py-1">About</li>
+          <li className="py-1 uppercase">About</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
         <NavLink to="/contact">
-          <li className="py-1">Contact</li>
+          <li className="py-1 uppercase">Contact</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
+        <a
+          target="_blank"
+          href={import.meta.env.VITE_ADMIN_URL}
+          className="border border-gray-200 px-5 text-xs py-1.5 rounded-full hover:bg-primary hover:text-white transition-all"
+        >
+          Admin Panel
+        </a>
       </ul>
       <div className="flex items-center gap-4">
         {token && userData ? (
